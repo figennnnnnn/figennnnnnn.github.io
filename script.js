8b3b6128b0899a91739cd644e6321d7626ecc2b9
FİGEN TEMEL <89043611+figennnnnnn@users.noreply.github.com>
2023-02-04 21:40:06 +0300

    *
    *

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typ",{
-    strings:["","Figen Temel","Biosystem Engineer"],
+    strings:["","Figen Temel","Biosystem Engineer","Web Designer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
